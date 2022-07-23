86d128cfcb564c487ef907e75b87b665d116baa4
danielle161 <danielle.print@xtra.co.nz>
2022-07-24 11:03:53 +1200

    Added random name generation

--- bus-thoughts/src/pages/Welcome.tsx
+++ bus-thoughts/src/pages/Welcome.tsx
@@ -1,12 +1,21 @@
 import { useHistory } from "react-router";
+import { uniqueNamesGenerator, Config, adjectives, animals } from 'unique-names-generator';
 
 const WelcomePage = () => {
   const history = useHistory();
+
+  const config: Config = {
+    dictionaries: [adjectives, animals],
+    separator: " "
+  }
+
   return (
     <div className="w-screen h-screen bg-primary text-white flex flex-col gap-10 items-center justify-center">
       <h1 className="font-bold text-[30px] text-white">Bus Besties</h1>
       <button
         onClick={() => {
+          const name: string = uniqueNamesGenerator(config)
+          window.sessionStorage.setItem("name", name);
           history.push("/thoughts");
           window.location.reload();
         }}
