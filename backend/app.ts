19281a658213c13d459c3ee1f85e3b62027848cc
wqsz7xn <wqsz7xn@protonmail.com>
2022-07-23 14:30:23 +1200

    add schemas

--- backend/app.ts
+++ backend/app.ts
@@ -1,47 +1,18 @@
 import {ApolloServer, gql} from 'apollo-server'
+import { messageResolver, messageSchema } from './schemas/message';
+import { busSchema, busResolver } from './schemas/bus';
+import { topicSchema, topicResolver } from './schemas/topic';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
+const baseTypeDefs = gql`
+  type Query
+`
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+//   typeDefs: [baseTypeDefs, Bus],
+  typeDefs: [busSchema, messageSchema, topicSchema],
+  resolvers: [busResolver, messageResolver, topicResolver],
   csrfPrevention: true,
+  introspection: true,
   cache: 'bounded',
 });
 
