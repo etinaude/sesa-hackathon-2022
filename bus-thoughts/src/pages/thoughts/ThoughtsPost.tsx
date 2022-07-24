6cc29d6c64fa6411bad4067ee878cf8ecc2cfad0
Raina Song <rainasong0509@gmail.com>
2022-07-24 14:41:00 +1200

    Fix merge conflicts

--- bus-thoughts/src/pages/thoughts/ThoughtsPost.tsx
+++ bus-thoughts/src/pages/thoughts/ThoughtsPost.tsx
@@ -49,7 +49,7 @@ const ThoughtsPost: React.FC = () => {
           },
         },
       });
-      hisotry.push("/*/tab3");
+      history.push("/*/tab3");
     } else {
       createMessage({
         variables: {
@@ -67,7 +67,7 @@ const ThoughtsPost: React.FC = () => {
   };
 
   const handleBack = () => {
-    hisotry.goBack();
+    history.goBack();
   };
 
   return (
